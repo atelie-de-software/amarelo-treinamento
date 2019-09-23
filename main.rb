401cbedc6387117282ea8c998a98deb0b3fc68bc
João Paulo <joao@webgoal.com.br>
2019-09-23 10:05:00 -0300

    Adiciona controle para movimentação do cursor

--- main.rb
+++ main.rb
@@ -3,8 +3,12 @@ require 'dispel'
 x = 0
 y = 0
 output = ''
+array = Array.new(10, 0)
+
 # draw app and redraw after each keystroke
 Dispel::Screen.open do |screen|
+  map = Dispel::StyleMap.new(3) # number of lines
+
   Dispel::Keyboard.output timeout: 0.5 do |key|
     screen.draw output
     next unless key
@@ -21,5 +25,7 @@ Dispel::Screen.open do |screen|
 
     output += "You pressed #{key}\n"
     output += "x: #{x} y: #{y}\n"
+
+    screen.draw "m", map, [x,y] # text, styles, cursor position
   end
 end
