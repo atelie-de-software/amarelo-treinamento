753581bed93fa9d7dcf7de15c71d16d622f16107
Webgoal <webgoal@Webgoals-iMac.local>
2019-09-24 09:51:23 -0300

    .

--- main.rb
+++ main.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'dispel'
 require './src/jogo'
 
@@ -7,15 +5,15 @@ jogo = Jogo.new
 # draw app and redraw after each keystroke
 Dispel::Screen.open do |screen|
   Dispel::Keyboard.output timeout: 0.5 do |key|
-    jogo.esquerda if key == :left
+    screen.draw jogo.tela
+    next unless key
+
+    exit(true) if key == :"Ctrl+c"
+
     jogo.direita if key == :right
+    jogo.esquerda if key == :left
     jogo.sobe if key == :up
     jogo.desce if key == :down
 
-
-    next unless key
-    next if key == :timeout
-
-    exit(true) if key == :"Ctrl+c"
   end
 end
