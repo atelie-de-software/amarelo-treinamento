4565e6a1eefc20ec2f96b40591a467317d2c2ad4
Webgoal <webgoal@Webgoals-iMac.local>
2019-09-24 09:28:26 -0300

    .

--- main.rb
+++ main.rb
@@ -1,30 +1,21 @@
 require 'dispel'
 require './src/jogo'
-x = 0
-y = 0
+
 jogo = Jogo.new
-array = Array.new(10, 0)
-TICK = 0.5
-OUTPUT = ''
-
+# draw app and redraw after each keystroke
 Dispel::Screen.open do |screen|
-  Dispel::Keyboard.output timeout: TICK do |key|
-  Curses.clear
-
+  Dispel::Keyboard.output timeout: 0.5 do |key|
+    screen.draw jogo.tela
     next unless key
 
     exit(true) if key == :"Ctrl+c"
 
-    y = jogo.jump(y) if key == :up
-    y = jogo.down(y) if key == :down
-    x = jogo.left(x) if key == :left
-    x = jogo.right(x) if key == :right
+    jogo.direita if key == :right
+    jogo.esquerda if key == :left
+    jogo.sobe if key == :up
+    jogo.desce if key == :down
 
-    write((y * -1), x, 'M')
+    screen.draw jogo.tela
+
   end
-end
-
-def write(line,row,text)
-  Curses.setpos(line,row)
-  Curses.addstr(text);
 end
