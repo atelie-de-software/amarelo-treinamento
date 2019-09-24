0fdf53bcede5459fbb873fc56b962ff444e7f1d4
Igor Corrêa <igor@webgoal.com.br>
2019-09-24 09:46:30 -0300

    Adiciona testes

--- spec/jogo_spec.rb
+++ spec/jogo_spec.rb
@@ -1,94 +1,101 @@
 require './src/jogo'
+
 RSpec.describe Jogo do
-  describe 'Tela inicial' do
-    xit 'Cenário inícial' do
-      jogo = Jogo.new
-      tela = "      \n"+
-             "    # \n"+
-             "   ? #\n"+
-             "      \n"+
-             "m_____\n"
-      expect(jogo.tela).to  eq(tela)
+  context 'Tela inicial' do
+    let(:jogo) { Jogo.new }
+    it 'Cenário inícial' do
+      tela = '      \n'\
+             '    # \n'\
+             '   ? #\n'\
+             '      \n'\
+             'm_____'
+      expect(jogo.tela).to eq(tela)
     end
 
-    xit 'Movimento para direita' do
-      jogo = Jogo.new
-      tela = "      \n"+
-             "    # \n"+
-             "   ? #\n"+
-             "      \n"+
-             "_m____\n"
-      
-      jogo.direita
+    it 'Movimento para direita' do
+      tela = '      \n'\
+             '    # \n'\
+             '   ? #\n'\
+             '      \n'\
+             '_m____'
 
-      expect(jogo.tela).to  eq(tela)
+      jogo.direita
+      expect(jogo.tela).to eq(tela)
     end
 
-    xit 'Movimentar cenário ao chegar na metade' do
-      jogo = Jogo.new
-      
-      jogo.direita
-      jogo.direita
-      jogo.direita
-      
-      tela = "      \n"+
-             "   #  \n"+
-             "  ? # \n"+
-             "      \n"+
-             "__m___\n"
+    it 'Movimento para a esquerda' do
+      tela = '      \n'\
+             '    # \n'\
+             '   ? #\n'\
+             '      \n'\
+             'm_____'
+      jogo.esquerda
+      expect(jogo.tela).to eq(tela)
+    end
 
-      expect(jogo.tela).to  eq(tela)
+    it 'Movimentar cenário ao chegar na metade' do
+      tela = '      \n'\
+             '   #  \n'\
+             '  ? # \n'\
+             '      \n'\
+             '__m___'
+
+      jogo.direita
+      jogo.direita
+      jogo.direita
+      expect(jogo.tela).to eq(tela)
     end
 
     xit 'Pulo sem obstáculo' do
-      jogo = Jogo.new
-      tela = "      \n"+
-             " m  # \n"+
-             "   ? #\n"+
-             "      \n"+
-             "______\n"
-      
-      jogo.direita
+      tela = '      \n'\
+             'm   # \n'\
+             '   ? #\n'\
+             '      \n'\
+             '______'
 
       jogo.sobe
-
-      expect(jogo.tela).to  eq(tela)
+      expect(jogo.tela).to eq(tela)
     end
-    
+
+    xit 'Desce sem obstáculo' do
+      tela = '      \n'\
+             '    # \n'\
+             '   ? #\n'\
+             '      \n'\
+             'm_____'
+      jogo.sobe
+      jogo.tick
+      expect(jogo.tela).to eq(tela)
+    end
+
     xit 'Pulo com obstáculo quebrável' do
-      jogo = Jogo.new
-      tela = "      \n"+
-             "  o#  \n"+
-             "  ! # \n"+
-             "  m   \n"+
-             "______\n"
-      
+      tela = '      \n'\
+             '  o#  \n'\
+             '  ! # \n'\
+             '  m   \n'\
+             '______'
+
       jogo.direita
       jogo.direita
       jogo.direita
       jogo.sobe
-
-      expect(jogo.tela).to  eq(tela)
+      expect(jogo.tela).to eq(tela)
     end
-    
+
     xit 'Pulo com obstáculo não quebrável' do
-      jogo = Jogo.new
-      tela = "      \n"+
-             " #    \n"+
-             "? #   \n"+
-             "  m   \n"+
-             "______\n"
-      
+      tela = '      \n'\
+             ' #    \n'\
+             '? #   \n'\
+             '  m   \n'\
+             '______'
+
       jogo.direita
       jogo.direita
       jogo.direita
       jogo.direita
       jogo.direita
       jogo.sobe
-
-
-      expect(jogo.tela).to  eq(tela)
+      expect(jogo.tela).to eq(tela)
     end
-    
   end
 end
