05b8e17c894c3e4ac9479fe6fa267637fe1c7be8
Webgoal <webgoal@Webgoals-iMac.local>
2019-09-24 11:03:50 -0300

    Refatora testes

--- spec/jogo_spec.rb
+++ spec/jogo_spec.rb
@@ -8,94 +8,130 @@ RSpec.describe Jogo do
              "    # \n"\
              "   ? #\n"\
              "      \n"\
-             'm_____'
+             "m_____"
       expect(jogo.tela).to eq(tela)
     end
 
-    xit 'Movimento para direita' do
-      jogo.direita
-      tela = "      \n"\
-             "    # \n"\
-             "   ? #\n"\
-             "      \n"\
-             '_m____'
-      expect(jogo.tela).to eq(tela)
+    context 'Movimentações do personagem' do
+      it 'Movimento para direita' do
+        jogo = Jogo.new
+        tela = "      \n"\
+               "    # \n"\
+               "   ? #\n"\
+               "      \n"\
+               "_m____"
+
+        jogo.direita
+        tela = "      \n"\
+               "    # \n"\
+               "   ? #\n"\
+               "      \n"\
+               '_m____'
+        expect(jogo.tela).to eq(tela)
+      end
+
+      context 'Quando personagem pular' do
+        it 'Sem obstáculo' do
+          jogo = Jogo.new
+          tela =  "      \n"\
+                  "m   # \n"\
+                  "   ? #\n"\
+                  "      \n"\
+                  "______"
+
+          jogo.sobe
+          expect(jogo.tela).to eq(tela)
+        end
+
+        xit 'Desce sem obstáculo' do
+          tela =  "      \n"\
+                  "    # \n"\
+                  "   ? #\n"\
+                  "      \n"\
+                  "m_____"
+          jogo.sobe
+          jogo.tick
+          expect(jogo.tela).to eq(tela)
+        end
+
+        xit 'Pulo com obstáculo quebrável' do
+          jogo = Jogo.new
+          tela =  "      \n"\
+                  "  o#  \n"\
+                  "  ! # \n"\
+                  "  m   \n"\
+                  "______"
+
+          jogo.direita
+          jogo.direita
+          jogo.direita
+          jogo.sobe
+          expect(jogo.tela).to eq(tela)
+        end
+
+        xit 'Pulo com obstáculo não quebrável' do
+          jogo = Jogo.new
+          tela =  "      \n"\
+                  " #    \n"\
+                  "? #   \n"\
+                  "  m   \n"\
+                  "______"
+
+          jogo.direita
+          jogo.direita
+          jogo.direita
+          jogo.direita
+          jogo.direita
+          jogo.sobe
+          expect(jogo.tela).to eq(tela)
+        end
+      end
     end
 
-    it 'Movimento para a esquerda' do
-      jogo.direita
+    context 'Personagem deve respeitar os limites da tela' do
+      xit 'Limite máximo para a lateral esquerda' do
+       tela = "      \n"\
+              "    # \n"\
+              "   ? #\n"\
+              "      \n"\
+              "m_____"
       jogo.esquerda
-      tela = "      \n"\
-             "    # \n"\
-             "   ? #\n"\
-             "      \n"\
-             'm_____'
       expect(jogo.tela).to eq(tela)
-    end
+      end
 
-    xit 'Movimentar cenário ao chegar na metade' do
-      tela = "      \n"\
-             "   #  \n"\
-             "  ? # \n"\
-             "      \n"\
-             '__m___'
+      xit 'Limite máximo para a lateral direita' do
+       tela = "      \n"\
+              "   #  \n"\
+              "  ? # \n"\
+              "      \n"\
+              "__m___"
 
-      jogo.direita
-      jogo.direita
-      jogo.direita
-      expect(jogo.tela).to eq(tela)
-    end
+       jogo.direita
+       jogo.direita
+       jogo.direita
+       jogo.direita
+       expect(jogo.tela).to eq(tela)
+      end
 
-    xit 'Pulo sem obstáculo' do
-      tela = "      \n"\
-             "m   # \n"\
-             "   ? #\n"\
-             "      \n"\
-             '______'
+      xit 'Limite máximo para baixo' do
+        tela = "      \n"\
+               "    # \n"\
+               "   ? #\n"\
+               "      \n"\
+               "m_____"
+        jogo.desce
+        expect(jogo.tela).to eq(tela)
+      end
 
-      jogo.sobe
-      expect(jogo.tela).to eq(tela)
-    end
-
-    xit 'Desce sem obstáculo' do
-      tela = "      \n"\
-             "    # \n"\
-             "   ? #\n"\
-             "      \n"\
-             'm_____'
-      jogo.sobe
-      jogo.tick
-      expect(jogo.tela).to eq(tela)
-    end
-
-    xit 'Pulo com obstáculo quebrável' do
-      tela = "      \n"\
-             "  o#  \n"\
-             "  ! # \n"\
-             "  m   \n"\
-             '______'
-
-      jogo.direita
-      jogo.direita
-      jogo.direita
-      jogo.sobe
-      expect(jogo.tela).to eq(tela)
-    end
-
-    xit 'Pulo com obstáculo não quebrável' do
-      tela = "      \n"\
-             " #    \n"\
-             "? #   \n"\
-             "  m   \n"\
-             '______'
-
-      jogo.direita
-      jogo.direita
-      jogo.direita
-      jogo.direita
-      jogo.direita
-      jogo.sobe
-      expect(jogo.tela).to eq(tela)
+      xit 'Limite máximo para cima' do
+        tela = "m     \n"\
+               "    # \n"\
+               "   ? #\n"\
+               "      \n"\
+               "______"
+        jogo.sobe
+        expect(jogo.tela).to eq(tela)
+      end
     end
   end
 end
