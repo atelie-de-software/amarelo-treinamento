08a9172b7fc087638ccd27ace9c698bdaae2282d
Igor Corrêa <igor@webgoal.com.br>
2019-09-24 11:33:13 -0300

    Pulo sem obstaculo

--- src/jogo.rb
+++ src/jogo.rb
@@ -2,10 +2,12 @@ class Jogo
   def initialize(posicao_x = 0, posicao_y = 4)
     @posicao_x = posicao_x
     @posicao_y = posicao_y
+    @contador_tick = 0
+    @subindo = false
   end
 
   def sobe
-    @posicao_y -= 3
+    @subindo = true
   end
 
   def desce
@@ -38,5 +40,12 @@ class Jogo
     campo.map { |linha| linha.reduce(:+) }.join("\n")
   end
 
-  def tick; end
+  def tick
+    if @subindo && @posicao_y > 1
+      @posicao_y -= 1
+      @subindo = false if @posicao_y == 1
+    elsif @posicao_y < 4 && @subindo == false
+      @posicao_y += 1
+    end
+  end
 end
