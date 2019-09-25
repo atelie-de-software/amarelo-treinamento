c0eadaffed1960186e73b09fcb5f6b6ccd35748c
João Paulo <joao@webgoal.com.br>
2019-09-25 10:38:38 -0300

    Refatoracao

--- main.rb
+++ main.rb
@@ -31,36 +31,12 @@ if resposta == 's'
     sleep(0.1)
 
     jogo.tick
-    jogo.tela.split("\n")[0].each_char.with_index { |c, index|
-      Image.new('images/mario.png', x: index * 25, y: 0)  if c == "m"
-      Image.new('images/Bloco_surpresa.png', x: index * 25, y: 0)  if c == "?"
-      Image.new('images/bloco_comum.png', x: index * 25, y: 0)  if c == "#"
-    }
-
-    jogo.tela.split("\n")[1].each_char.with_index { |c, index|
-      Image.new('images/mario.png', x: index * 25, y: 25)  if c == "m"
-      Image.new('images/Bloco_surpresa.png', x: index * 25, y: 25)  if c == "?"
-      Image.new('images/bloco_comum.png', x: index * 25, y: 0)  if c == "#"
-
-    }
-
-    jogo.tela.split("\n")[2].each_char.with_index { |c, index|
-      Image.new('images/mario.png', x: index * 25, y: 50)  if c == "m"
-      Image.new('images/Bloco_surpresa.png', x: index * 25, y: 50)  if c == "?"
-      Image.new('images/bloco_comum.png', x: index * 25, y: 0)  if c == "#"
-
-    }
-
-    jogo.tela.split("\n")[3].each_char.with_index { |c, index|
-      Image.new('images/mario.png', x: index * 25, y: 75)  if c == "m"
-      Image.new('images/Bloco_surpresa.png', x: index * 25, y: 75)  if c == "?"
-      Image.new('images/bloco_comum.png', x: index * 25, y: 0)  if c == "#"
-    }
-
-    jogo.tela.split("\n")[4].each_char.with_index { |c, index|
-      Image.new('images/mario.png', x: index * 25, y: 100)  if c == "m"
-      Image.new('images/Bloco_surpresa.png', x: index * 25, y: 100)  if c == "?"
-      Image.new('images/bloco_comum.png', x: index * 25, y: 0)  if c == "#"
+    jogo.tela.split("\n").each.with_index { |line, index_line|
+      line.each_char.with_index { |c, index|
+        Image.new('images/mario.png', x: index * 25, y: index_line * 25)  if c == "m"
+        Image.new('images/Bloco_surpresa.png', x: index * 25, y: index_line * 25)  if c == "?"
+        Image.new('images/bloco_comum.png', x: index * 25, y: index_line * 25) if c == "#"
+      }
     }
 
   end
