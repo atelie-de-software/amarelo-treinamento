13eac745214945335242b8f5d1a53645375bf951
Raphael Martins <raphael@webgoal.com.br>
2019-09-26 10:32:50 -0300

    adiciona imagens do mario grande e do inimigo

--- src/modo_grafico.rb
+++ src/modo_grafico.rb
@@ -40,8 +40,10 @@ end
 
 def plotar_imagem c, position_x, position_y
   Image.new('images/mario_direita_1.png', x: position_x, y: position_y) if c == "m"
+  Image.new('images/mario_grande_direita_1.png', x: position_x, y: position_y) if c == "M"
   Image.new('images/bloco_surpresa_fechado.png', x: position_x, y: position_y) if c == "?"
   Image.new('images/bloco_comum.png', x: position_x, y: position_y) if c == "#"
   Image.new('images/bloco_surpresa_aberto.png', x: position_x, y: position_y) if c == "!"
   Image.new('images/cogumelo.png', x: position_x, y: position_y) if c == "o"
+  Image.new('images/inimigo_1_vivo.png', x: position_x, y: position_y) if c == "w"
 end
