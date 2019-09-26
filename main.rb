c4555bac1cd808eef7a698f1f1ff4c0f6b9040c0
Raphael Martins <raphael@webgoal.com.br>
2019-09-26 09:52:39 -0300

    a

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require './src/modo_grafico'
 require './src/modo_texto'
 
-puts "Deseja iniciar o modo grafico? s/n"
+puts 'Deseja iniciar o modo grafico? s/n'
 if gets[0] == 's'
   jogar_modo_grafico
 else
